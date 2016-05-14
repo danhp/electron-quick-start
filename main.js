da1d6b228a863715a39b2cc0807e8efa309e06ba
Daniel Pham <pham.dany@gmail.com>
2016-05-14 16:21:10 -0400

    checkbox bug test

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const app = electron.app
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
+const {Menu, MenuItem} = electron
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
@@ -15,15 +17,21 @@ function createWindow () {
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`)
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  const page = mainWindow.webContents
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
+  const menu = new Menu();
+  menu.append(new MenuItem(
+      {
+        label: 'Test',
+        type: 'checkbox',
+        click(item, win) {
+          console.log(item.checked)
+        }
+      }
+  ))
+
+  page.on('context-menu', (e, params) => {
+    menu.popup(mainWindow)
   })
 }
 
