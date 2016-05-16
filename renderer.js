78ab57b4a9f398fc7e04d0bfa2ed2ed60e497280
Daniel Pham <pham.dany@gmail.com>
2016-05-16 18:27:25 -0400

    Demo that bug is not in Electron

--- renderer.js
+++ renderer.js
@@ -1,3 +1,8 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+var webFrame = require('electron').webFrame;
+var SpellCheckProvider = require('electron-spell-check-provider');
+
+webFrame.setSpellCheckProvider('en-US', true, new SpellCheckProvider('en-US'));
